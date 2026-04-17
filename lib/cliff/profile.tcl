f52e83f8e2638b533eb1eefc0eef80f1a88866fb
Ian Smith <197130+imsmith@users.noreply.github.com>
2026-04-17 12:48:54 -0700

    profile: support describe, image, workspace, home directives

--- lib/cliff/profile.tcl
+++ lib/cliff/profile.tcl
@@ -5,7 +5,12 @@ namespace eval ::cliff::profile {
         variable _state
         set _state [dict create]
         set slave [interp create -safe]
-        $slave alias image ::cliff::profile::_set image
+        foreach cmd {describe image} {
+            $slave alias $cmd ::cliff::profile::_set $cmd
+        }
+        foreach cmd {workspace home} {
+            $slave alias $cmd ::cliff::profile::_set_kv $cmd
+        }
         $slave eval $src
         interp delete $slave
         return $_state
@@ -14,5 +19,17 @@ namespace eval ::cliff::profile {
     proc _set {key value} {
         variable _state
         dict set _state $key $value
+    }
+
+    proc _set_kv {key args} {
+        variable _state
+        set m [dict create]
+        foreach pair $args {
+            if {![regexp {^([^=]+)=(.*)$} $pair -> k v]} {
+                error "profile: expected key=value, got: $pair"
+            }
+            dict set m $k $v
+        }
+        dict set _state $key $m
     }
 }
